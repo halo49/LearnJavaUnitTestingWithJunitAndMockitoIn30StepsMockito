7105c40d8f0223566cd9b709232b41ee9b7428a6
halo49 <83737814+halo49@users.noreply.github.com>
2024-06-13 13:25:40 -0600

    Capturing arguments passed to a Mock

--- src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
+++ src/test/java/com/in28minutes/business/TodoBusinessImplMockTest.java
@@ -10,12 +10,14 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import com.in28minutes.data.api.TodoService;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.never;
 import static org.mockito.BDDMockito.times;
 import static org.mockito.BDDMockito.atLeast;
+import static org.mockito.BDDMockito.then;
 import static org.hamcrest.CoreMatchers.is;
 
 public class TodoBusinessImplMockTest {
@@ -106,5 +108,97 @@ public class TodoBusinessImplMockTest {
 		verify(todoServiceMock, never()).deleteTodo("Learn Spring");
 	}
 
+	@Test
+	public void testDeleteTodosNotRelatedToSpring_usingBDD_v2() {
+		// Given
+		TodoService todoServiceMock = mock(TodoService.class);
+		
+		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+
+		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
+		
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		
+		// When
+		todoBusinessImpl
+			.deleteTodosNotRelatedToSpring("Dummy");
+		
+		// Then
+		//verify(todoServiceMock).deleteTodo("Learn to Dance");
+		then(todoServiceMock).should().deleteTodo("Learn to Dance");
+		
+		//verify(todoServiceMock, never()).deleteTodo("Learn Spring MVC");
+		then(todoServiceMock).should(never()).deleteTodo("Learn Spring MVC");
+		
+		//verify(todoServiceMock, never()).deleteTodo("Learn Spring");
+		then(todoServiceMock).should(never()).deleteTodo("Learn Spring");
+	}
 	
+	@Test
+	public void testDeleteTodosNotRelatedToSpring_usingBDD_argumentCapture() {
+		
+		// Declare Argument Capture
+		ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+		
+		// Define Argument captor on specific method call
+		// Capture the argument
+		
+		// Given
+		TodoService todoServiceMock = mock(TodoService.class);
+		
+		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+
+		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
+		
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		
+		// When
+		todoBusinessImpl
+			.deleteTodosNotRelatedToSpring("Dummy");
+		
+		// Then
+		then(todoServiceMock).should().deleteTodo(stringArgumentCaptor.capture());
+		
+		assertThat(stringArgumentCaptor.getValue(), is("Learn to Dance"));
+
+	}
+	
+	@Test
+	public void testDeleteTodosNotRelatedToSpring_usingBDD_argumentCaptureMultipleTimes() {
+		
+		// Declare Argument Capture
+		ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+		
+		// Define Argument captor on specific method call
+		// Capture the argument
+		
+		// Given
+		TodoService todoServiceMock = mock(TodoService.class);
+		
+		List<String> todos = Arrays.asList("Learn to Rock and Roll", "Learn Spring", "Learn to Dance");
+
+		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
+		
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		
+		// When
+		todoBusinessImpl
+			.deleteTodosNotRelatedToSpring("Dummy");
+		
+		// Then
+		then(todoServiceMock).should(times(2)).deleteTodo(stringArgumentCaptor.capture());
+		
+		// Si se cambia "Learn Spring MVC" a "Learn to Rock and Roll" entonces
+		// sucederan dos llamados al metodo delete y esta linea marcara error, ya
+		// que solo espera una llamada
+		//assertThat(stringArgumentCaptor.getValue(), is("Learn to Dance"));
+		// Para solucionar lo anterior se cambia el getValue() por getAllValues() 
+		// y arriba se cambia el el should() por el should(times(2))
+		// El test prueba solo "Learn to Dance" para hacer un test que funcione
+		// cambiamos esta linea
+		//assertThat(stringArgumentCaptor.getAllValues(), is("Learn to Dance"));
+		// por esta otra
+		assertThat(stringArgumentCaptor.getAllValues().size(), is(2));
+
+	}
 }
